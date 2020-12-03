3ee385c1a5cc44ed1580d45f99cdde0f780118a6
jensnti <jens.andreasson@ga.ntig.se>
2020-12-03 08:42:37 +0100

    created files, copied some content from template, it builds and runs

---
+++ src/gameConfig.js
@@ -0,0 +1,14 @@
+import Phaser from 'phaser';
+import BootScene from './scenes/Boot';
+import PlayScene from './scenes/Play';
+import MenuScene from './scenes/Menu';
+import EndScene from './scenes/End';
+
+export default {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  title: 'Exempel med parcel',
+  banner: { text: 'white', background: ['#FD7400', '#FFE11A', '#BEDB39', '#1F8A70', '#004358'] },
+  scene: [BootScene, MenuScene, PlayScene, EndScene]
+};
